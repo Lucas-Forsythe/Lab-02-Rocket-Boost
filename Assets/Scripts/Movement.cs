d5d634e3af70b0b197027673f1145fd9e0aa6008
Lucas Forsythe <lucasforsythe2004@gmail.com>
2026-09-21 13:52:42 -0400

    Added rocket rotation, camera, and physics tuning

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] private float thrustStrength = 1000f;
+    [SerializeField] private float rotationStrength = 100f;
 
     Rigidbody rb;
 
@@ -15,13 +17,40 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
     {
-        if(thrust.IsPressed())
+        ProcccessThrust();
+        ProccessRotation();
+    }
+
+    private void ProcccessThrust()
+    {
+        if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProccessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if(rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false; // unfreezing rotation so the physics system can take over
     }
 }
